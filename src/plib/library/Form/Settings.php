96a81a656871bd6620ae3a5173ed17d93ec9905c
Vladimir Likhtanskiy <vlikhtanskiy@plesk.com>
2018-03-05 15:50:22 +0700

    Add ability to create restricted IAM user using root credentials

--- src/plib/library/Form/Settings.php
+++ src/plib/library/Form/Settings.php
@@ -1,20 +1,48 @@
 <?php
 // Copyright 1999-2016. Parallels IP Holdings GmbH.
+
+require_once (__DIR__ . '/../../vendor/autoload.php');
+
 class Modules_Route53_Form_Settings extends pm_Form_Simple
 {
+    const USERNAME_PREFIX = 'plesk-route53-';
+    const USER_POLICY_ACTIONS = ['route53:*', 'route53domains:*'];
+
+    const KEY_TYPE_ROOT_CREDENTAL = 'rootCredential';
+    const KEY_TYPE_USER_CREDENTAL = 'userCredential';
+
     public function init()
     {
         parent::init();
 
         $this->addElement('description', 'description', [
-             'description' => $this->lmsg('getAuth') . ' : ' .
-                '<a href="https://portal.aws.amazon.com/" target="_blank">portal.aws.amazon.com</a>' .
-                '-&gt; MyAccount -&gt; Security Credentials',
-             'escape' => false,
+            'description' =>
+                '<ul>' .
+
+            '<li>' . pm_Locale::lmsg('getAuth') . ' : ' .
+            '<a href="https://aws.amazon.com/" target="_blank">https://aws.amazon.com</a> ' .
+            '-&gt; MyAccount -&gt; Security Credentials </li>' .
+            '<li>' . pm_Locale::lmsg('getAuthStepTwo', [
+                'learnMoreUrl' => 'https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/access-control-managing-permissions.html'
+             ]) . '</li>',
+            'escape' => false,
+        ]);
+
+        $this->addElement('radio', 'keyType', [
+            'label' => pm_Locale::lmsg('awsKeyType'),
+            'multiOptions' => [
+                self::KEY_TYPE_ROOT_CREDENTAL => pm_Locale::lmsg('formRootCredential'),
+                self::KEY_TYPE_USER_CREDENTAL => pm_Locale::lmsg('formPreCreatedLimitedUserCredential')
+            ],
+            'value' => pm_Settings::get('keyType', self::KEY_TYPE_ROOT_CREDENTAL),
+            'required' => true,
+            'validators' => [
+                ['NotEmpty', true],
+            ],
         ]);
 
         $this->addElement('text', 'key', array(
-            'label' => $this->lmsg('keyLabel'),
+            'label' => pm_Locale::lmsg('keyLabel'),
             'value' => pm_Settings::get('key'),
             'class' => 'f-large-size',
             'required' => true,
@@ -23,7 +51,7 @@ class Modules_Route53_Form_Settings extends pm_Form_Simple
             ),
         ));
         $this->addElement('text', 'secret', array(
-            'label' => $this->lmsg('secretLabel'),
+            'label' => pm_Locale::lmsg('secretLabel'),
             'value' => pm_Settings::get('secret'),
             'class' => 'f-large-size',
             'required' => true,
@@ -32,11 +60,12 @@ class Modules_Route53_Form_Settings extends pm_Form_Simple
             ),
         ));
         $this->addElement('checkbox', 'enabled', array(
-            'label' => $this->lmsg('enabledLabel'),
+            'label' => pm_Locale::lmsg('enabledLabel'),
             'value' => pm_Settings::get('enabled'),
         ));
 
         $this->addControlButtons(array(
+            'sendTitle' => pm_Locale::lmsg('login'),
             'cancelLink' => pm_Context::getModulesListUrl(),
         ));
     }
@@ -45,12 +74,20 @@ class Modules_Route53_Form_Settings extends pm_Form_Simple
     {
         if ($data['enabled']) {
             try {
-                Modules_Route53_Client::factory([
-                    'credentials' => [
-                        'key' => $data['key'],
-                        'secret' => $data['secret'],
-                    ],
-                ])->checkCredentials();
+                if ($data['keyType'] == self::KEY_TYPE_ROOT_CREDENTAL) {
+                    $res = $this->isAdministratorAccess($data['key'], $data['secret']);
+                    if (!$res) {
+                        throw new Exception(pm_Locale::lmsg('notAdministratorAccess'));
+                    }
+                } else {
+                    Modules_Route53_Client::factory([
+                        'credentials' => [
+                            'key' => $data['key'],
+                            'secret' => $data['secret'],
+                        ]
+                    ])->checkCredentials();
+                }
+
             } catch (Exception $e) {
                 $this->markAsError();
                 $this->getElement('key')->addError($e->getMessage());
@@ -67,8 +104,80 @@ class Modules_Route53_Form_Settings extends pm_Form_Simple
 
     public function process()
     {
-        pm_Settings::set('key', $this->getValue('key'));
-        pm_Settings::set('secret', $this->getValue('secret'));
+        $res = [];
+
         pm_Settings::set('enabled', $this->getValue('enabled'));
+
+        $keyType = $this->getValue('keyType');
+        $key = $this->getValue('key');
+        $secret = $this->getValue('secret');
+
+        if ($keyType == self::KEY_TYPE_USER_CREDENTAL) {
+            $this->saveUserData($key, $secret);
+        } else {
+            $res = $this->createUser($key, $secret);
+            if ($res) {
+                $this->saveUserData($res['key'], $res['secret']);
+            }
+        }
+        return $res;
+    }
+
+    private function saveUserData($key, $secret)
+    {
+        pm_Settings::set('key', $key);
+        pm_Settings::set('secret', $secret);
+        pm_Settings::set('keyType', self::KEY_TYPE_USER_CREDENTAL);
+    }
+
+    private function isAdministratorAccess($key, $secret)
+    {
+        $iamComponent = new AmazonIAM();
+        $iamComponent
+            ->setKey($key)
+            ->setSecret($secret)
+        ;
+        $res = $iamComponent->isAdministratorAccess();
+        return $res;
+    }
+
+    /**
+     * @param $key
+     * @param $secret
+     *
+     * @return array|bool
+     */
+    private function createUser($key, $secret)
+    {
+        $res = false;
+        try {
+            $iamComponent = new AmazonIAM();
+            $iamComponent->setKey($key)->setSecret($secret);
+            $userName = $iamComponent->generateIAMUserName(self::USERNAME_PREFIX);
+
+            $iamComponent->createUser($userName);
+
+            $policyDocument = $iamComponent->createPolicyDocument(self::USER_POLICY_ACTIONS);
+
+            $iamComponent->getIAMClient()
+                ->putUserPolicy([
+                'PolicyDocument' => $policyDocument->__toString(),
+                'PolicyName' => self::USERNAME_PREFIX .'full-access',
+                'UserName' => $userName
+            ]);
+
+            $response = $iamComponent->createAccessKey($userName);
+            $responseAccessKey = $response->get('AccessKey');
+            pm_View_Status::addInfo(pm_Locale::lmsg('iamUserCreated', ['userName' => $userName]));
+            $res = [
+                'userName' => $userName,
+                'key' => $responseAccessKey['AccessKeyId'],
+                'secret' => $responseAccessKey['SecretAccessKey'],
+            ];
+        } catch (\Aws\Exception\AwsException $e) {
+            pm_View_Status::addError($e->getAwsErrorMessage());
+        }
+
+        return $res;
     }
 }
--- src/plib/scripts/post-install.php
+++ src/plib/scripts/post-install.php
@@ -2,6 +2,12 @@
 // Copyright 1999-2016. Parallels IP Holdings GmbH.
 pm_Loader::registerAutoload();
 pm_Context::init('route53');
+$keyType = pm_Settings::get('keyType');
+$key = pm_Settings::get('key');
+
+if (empty($keyType) && !empty($key)) {
+    pm_Settings::set('keyType', Modules_Route53_Form_Settings::KEY_TYPE_USER_CREDENTAL);
+}
 
 try {
     if (substr(PHP_OS, 0, 3) == 'WIN') {
