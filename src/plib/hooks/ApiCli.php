41ae4ed630021b9072b05db32a4da67b6d4db54d
Vladimir Likhtanskiy <vlikhtanskiy@plesk.com>
2018-05-11 18:58:11 +0700

    Use environment variable for sensitive parameters

--- src/plib/hooks/ApiCli.php
+++ src/plib/hooks/ApiCli.php
@@ -2,20 +2,43 @@
 // Copyright 1999-2018. Plesk International GmbH.
 class Modules_Route53_ApiCli extends pm_Hook_ApiCli
 {
-    public function initCommand($root, $user, $accessKey, $secretKey, $clear)
+    public function initCommand($root, $user, $clear, $accessKey = '', $secretKey = '')
     {
         if (isset($clear)) {
             $this->clear();
             return;
         }
         if (isset($root)) {
-            $this->initUserCredentials($accessKey, $secretKey, Modules_Route53_Form_Settings::KEY_TYPE_ROOT_CREDENTAL);
+            $this->initUserCredentials(Modules_Route53_Form_Settings::KEY_TYPE_ROOT_CREDENTAL, $accessKey, $secretKey);
             return;
         }
         if (isset($user)) {
-            $this->initUserCredentials($accessKey, $secretKey, Modules_Route53_Form_Settings::KEY_TYPE_USER_CREDENTAL);
+            $this->initUserCredentials(Modules_Route53_Form_Settings::KEY_TYPE_USER_CREDENTAL, $accessKey, $secretKey);
             return;
         }
+        $this->stderr(pm_Locale::lmsg('cli.commands.init.wrongSyntax'));
+        $this->exitCode(1);
+    }
+
+    public function getAvailableOptions($command)
+    {
+        $options = parent::getAvailableOptions($command);
+        if (pm_ProductInfo::isUnix() && 'init' == $command) {
+            $options = array_diff($options, ['access-key', 'secret-key']);
+        }
+        return $options;
+    }
+
+    public function helpCommand()
+    {
+        ob_start();
+        parent::helpCommand();
+        $help = ob_get_contents();
+        ob_clean();
+
+        $initCommandKey = pm_ProductInfo::isUnix() ? 'Unix' : 'Win';
+        $help = str_replace('[[cli.commands.init]]', pm_Locale::lmsg("cli.commands.init{$initCommandKey}"), $help);
+        $this->stdout($help);
     }
 
     /**
@@ -23,8 +46,12 @@ class Modules_Route53_ApiCli extends pm_Hook_ApiCli
      * @param $secretKey
      * @param $keyType
      */
-    private function initUserCredentials($accessKey, $secretKey, $keyType)
+    private function initUserCredentials($keyType, $accessKey, $secretKey)
     {
+        if (pm_ProductInfo::isUnix()) {
+            $accessKey = getenv('ACCESS_KEY');
+            $secretKey = getenv('SECRET_KEY');
+        }
         $settingsForm = new Modules_Route53_Form_Settings([
             'isConsole' => true
         ]);
@@ -39,9 +66,21 @@ class Modules_Route53_ApiCli extends pm_Hook_ApiCli
         if ($settingsForm->isValid($params)) {
             $res = $settingsForm->process();
             if ($keyType == Modules_Route53_Form_Settings::KEY_TYPE_USER_CREDENTAL) {
-                $this->stdout( pm_Locale::lmsg('userLoggedIn') . PHP_EOL);
+                $this->stdout(pm_Locale::lmsg('userLoggedIn') . PHP_EOL);
             } else {
-                $this->stdout(pm_Locale::lmsg('iamUserCreated', ['userName' => $res['userName']]) . PHP_EOL);
+                if ($res) {
+                    $this->stdout(pm_Locale::lmsg('iamUserCreated', ['userName' => $res['userName']]) . PHP_EOL);
+                } else {
+                    $message = '';
+                    foreach (pm_View_Status::getAllMessages(false) as $msg) {
+                        if ($msg['status'] != pm_View_Status::STATUS_ERROR) {
+                            continue;
+                        }
+                        $message .= $msg['content'] . PHP_EOL;
+                    }
+                    $this->stderr($message);
+                    $this->exitCode(1);
+                }
             }
 
         } else {
@@ -62,6 +101,6 @@ class Modules_Route53_ApiCli extends pm_Hook_ApiCli
     private function clear()
     {
         pm_Settings::clean();
-        $this->stdout( pm_Locale::lmsg('cliClearSuccess') . PHP_EOL);
+        $this->stdout(pm_Locale::lmsg('cliClearSuccess') . PHP_EOL);
     }
 }
