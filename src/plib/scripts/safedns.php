0434305aecfc56a427b36757bfddfee92686c6d1
root <root@plesk-demo1.novalocal>
2020-01-23 23:28:11 +0000

    created safedns.php

---
+++ src/plib/scripts/safedns.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * This is the SafeDNS Integration Script
+ *
+ * Documentation Link
+ */
+
+
+
+/**
+ * Read zone script from stdin
+ *
+ *[
+ * {
+ *  "command": "(update|delete)",
+ *  "zone": {
+ *      "name": "domain.tld.",
+ *      "displayName": "domain.tld.",
+ *      "soa": {
+ *          "email": "email@address",
+ *          "status": 0,
+ *          "type": "master",
+ *          "ttl": 86400,
+ *          "refresh": 10800,
+ *          "retry": 3600,
+ *          "expire": 604800,
+ *          "minimum": 10800,
+ *          "serial": 123123123,
+ *          "serial_format": "UNIXTIMESTAMP"
+ *      },
+ *      "rr": [{
+ *          "host": "www.domain.tld.",
+ *          "displayHost": "www.domain.tld.",
+ *          "type": "CNAME",
+ *          "displayValue": "domain.tld.",
+ *          "opt": "",
+ *          "value": "domain.tld."
+ *      }]
+ * }, {
+ *  "command": "(createPTRs|deletePTRs)",
+ *  "ptr": {
+ *      "ip_address": "1.2.3.4",
+ *      "hostname": "domain.tld"}
+ * }
+ *]
+ */
+
+/*
+
+$data = json_decode(file_get_contents('php://stdin'));
+//Example:
+//[
+//    {"command": "update", "zone": {"name": "domain.tld.", "displayName": "domain.tld.", "soa": {"email": "amihailov@parallels.com", "status": 0, "type": "master", "ttl": 86400, "refresh": 10800, "retry": 3600, "expire": 604800, "minimum": 10800, "serial": 1363228965, "serial_format": "UNIXTIMESTAMP"}, "rr": [
+//        {"host": "www.domain.tld.", "displayHost": "www.domain.tld.", "type": "CNAME", "displayValue": "domain.tld.", "opt": "", "value": "domain.tld."},
+//        {"host": "1.2.3.4", "displayHost": "1.2.3.4", "type": "PTR", "displayValue": "domain.tld.", "opt": "24", "value": "domain.tld."},
+//        {"host": "domain.tld.", "displayHost": "domain.tld.", "type": "TXT", "displayValue": "v=spf1 +a +mx -all", "opt": "", "value": "v=spf1 +a +mx -all"},
+//        {"host": "ftp.domain.tld.", "displayHost": "ftp.domain.tld.", "type": "CNAME", "displayValue": "domain.tld.", "opt": "", "value": "domain.tld."},
+//        {"host": "ipv4.domain.tld.", "displayHost": "ipv4.domain.tld.", "type": "A", "displayValue": "1.2.3.4", "opt": "", "value": "1.2.3.4"},
+//        {"host": "mail.domain.tld.", "displayHost": "mail.domain.tld.", "type": "A", "displayValue": "1.2.3.4", "opt": "", "value": "1.2.3.4"},
+//        {"host": "domain.tld.", "displayHost": "domain.tld.", "type": "MX", "displayValue": "mail.domain.tld.", "opt": "10", "value": "mail.domain.tld."},
+//        {"host": "webmail.domain.tld.", "displayHost": "webmail.domain.tld.", "type": "A", "displayValue": "1.2.3.4", "opt": "", "value": "1.2.3.4"},
+//        {"host": "domain.tld.", "displayHost": "domain.tld.", "type": "A", "displayValue": "1.2.3.4", "opt": "", "value": "1.2.3.4"},
+//        {"host": "ns.domain.tld.", "displayHost": "ns.domain.tld.", "type": "A", "displayValue": "1.2.3.4", "opt": "", "value": "1.2.3.4"}
+//    ]}},
+//    {"command": "createPTRs", "ptr": {"ip_address": "1.2.3.4", "hostname": "domain.tld"}},
+//    {"command": "createPTRs", "ptr": {"ip_address": "2002:5bcc:18fd:000c:0001:0002:0003:0004", "hostname": "domain.tld"}}
+//]
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+*/
+
+function call_SafeDNS_API($method, $url, $data){
+   $curl = curl_init();
+   switch ($method){
+      case "POST":
+         curl_setopt($curl, CURLOPT_POST, 1);
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      case "GET":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+         break;
+      case "PATCH":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PATCH");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      case "DELETE":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+
+      default:
+         if ($data)
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+   }
+   // OPTIONS:
+   curl_setopt($curl, CURLOPT_URL, $url);
+   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Authorization: ECf4UuwBe8CvmpvnparbtS01cWaULY3u',
+      'Content-Type: application/json',
+   ));
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+   // EXECUTE:
+   $result = curl_exec($curl);
+   if(!$result){die("API Connection Failure \n");}
+
+   $responsecode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//   echo "Response code : ".$responsecode;
+// TODO - If response code not 200 , handle
+   curl_close($curl);
+
+
+   return $result;
+}
+
+$api_url="https://api.ukfast.io/safedns/v1";
+
+function request_safedns_zones(){
+    $get_data = call_SafeDNS_API('GET',"https://api.ukfast.io/safedns/v1/zones",false);
+    $response = json_decode($get_data, true);
+    $data = $response;
+    echo(" request zones data ".json_encode($data));
+}
+
+request_safedns_zones();
+
+
+/* EXAMPLE PROGRAM LOOP
+foreach ($data as $record) {
+
+    $zoneName = $record->zone->name;
+    $recordsTTL = $record->zone->soa->ttl;
+    switch ($record->command) {
+        
+        case 'create':
+        case 'update':
+
+            // If zone does not exist, create it
+            /* For records in zone:
+                 - Check if record is present in safedns
+                 - If record is not present, create it.
+                 - If record is present, check if it needs ,and update
+                 - If record is present in safedns, but has been removed from plesk, delete it
+
+        case 'delete':
+             // Delete a zone
+              
+    }
+}*/
