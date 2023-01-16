2b4e600813751fbb9bd6f4dfd799773bedf9e0d7
Maksim Savrilov <msavrilov@plesk.com>
2023-01-16 16:11:54 +0200

    EXTPLESK-4242 Add PHP Scoper

--- scoper.inc.php
+++ scoper.inc.php
@@ -24,6 +24,23 @@ return [
             ])
             ->in('src/plib'),
     ],
+    'patchers' => [
+        function (string $filePath, string $prefix, string $contents): string {
+            if (preg_match("#/aws-sdk-php/src/Sdk.php$#", $filePath)
+                || preg_match("#/aws-sdk-php/src/AwsClient.php$#", $filePath)
+                || preg_match("#/aws-sdk-php/src/MultiRegionClient.php$#", $filePath)
+            ) {
+                return preg_replace("#\"Aws\\\\#", "\"PleskRoute53\\\\\\\\Aws\\", $contents);
+            }
+            return $contents;
+        },
+        function (string $filePath, string $prefix, string $contents): string {
+            if (preg_match("#/aws-sdk-php/src/Signature/SignatureV4.php$#", $filePath)) {
+                return preg_replace("#ISO8601_BASIC = \'[a-zA-Z0-9\\\]{1,}#", "ISO8601_BASIC = 'Ymd\THis\Z", $contents);
+            }
+            return $contents;
+        },
+    ],
     'exclude-namespaces' => [
         '~^Plesk~',
         'Psr',
